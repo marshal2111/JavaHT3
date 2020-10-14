23b83cb9a5081dcc2cb02e4449b71cfa033941cd
ilya <zaporozhecilya@gmail.com>
2020-10-14 23:48:36 +0300

    Realized Randomizer

--- HT3/src/Main.java
+++ HT3/src/Main.java
@@ -3,17 +3,20 @@ package lab2;
 public class Main {
 
 	public static void main(String[] args) {
-	    MyList<Integer> list = new MyList<>((a, b) -> {
-	    	if (((int) a.getValue() - (int) b.getValue()) > 0) {
+	    /*MyList<Integer> list = new MyList<>((a, b) -> {
+	    	if (( (int)a.getValue() - (int)b.getValue()) > 0) {
 	    		return 1;	
 	    	} else if (((int) a.getValue() - (int) b.getValue()) == 0) {
 	    		return 0;
 	    	}
 	    	return -1;
-	    });
-	    /*MyList<Integer> list = new MyList<>((a, b) -> {if((int)a.getValue() - (int)b.getValue() > 0)
-        return 1; else if ((int)a.getValue() - (int)b.getValue() < 0) return -1; else return 0;});  */
-	    Randomizer.randomFill(list, 100);
+	    });*/
+	    MyList<String> list = new MyList<>((a, b) -> {return 1;});
+	    list.add(Randomizer.random_string(50));
+	    list.add(Randomizer.random_string(10));
+	    list.add(Randomizer.random_string(30));
+	    //Randomizer.randomFill_int(list, 100);
+	    //list.Sort();
         System.out.println(list.toString());
    	}
 }
--- HT3/src/Randomizer.java
+++ HT3/src/Randomizer.java
@@ -1,9 +1,10 @@
 package lab2;
 
 import java.util.Collection;
+import java.io.*;
 
 public class Randomizer {
-    public static boolean randomFill(Collection<Integer> collection, long numbers) {
+    public static boolean randomFill_int(Collection<Integer> collection, long numbers) {
         if (numbers <= 0)
         {
             return false;
@@ -12,5 +13,26 @@ public class Randomizer {
             collection.add((int)(Math.random() * 100));
         }
         return true;
+    }
+    public static boolean randomFill_double(Collection<Double> collection, long numbers) {
+        if (numbers <= 0)
+        {
+            return false;
+        }
+        for (int i = 0; i < numbers; i++) {
+            collection.add((Math.random() * 100));
+        }
+        return true;
+    }
+    public static String random_string(int max_length) {
+        int length = Math.max(1, (int)(Math.random() * max_length));
+        StringBuilder str = new StringBuilder("");
+        while (str.length() < length) {
+            int b = 65 + (int)(Math.random() * 57);
+            if ((b > 96) || (b < 91))
+                str.append((char)b);
+        }
+        String res = str.toString();
+        return res;
     }
 }
